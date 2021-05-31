d013269f566bd795bd1204be8366f9e270b0633e
陈小术 <n551wv1@163.com>
2021-05-31 16:21:30 +0800

    ✨ 优化界面

--- pages/components/blank-bar/blank-bar.vue
+++ pages/components/blank-bar/blank-bar.vue
@@ -1,5 +1,10 @@
 <template>
 	<view>
+		<c-blank-bar background="#f0f0f0" :is-fixed="true">
+			<view class="pl-5 pr-5 text-df">
+				这是固定的空白栏
+			</view>
+		</c-blank-bar>
 		<view class="c-demo">
 			<view class="c-demo-wrap mt-30">
 				<view class="c-demo-title">演示效果</view>
@@ -10,8 +15,10 @@
 				</view>
 			</view>
 		</view>
-		<c-blank-bar background="#dddddd" :is-fixed="true" :is-bottom="true" safeAreaInsetBottom>
-			<button type="default">提交</button>
+		<c-blank-bar background="#f0f0f0" :is-fixed="true" :is-bottom="true" safeAreaInsetBottom>
+			<view class="pl-5 pr-5">
+				<button :style="[{background: main_color, color: '#FFF'}]" class="c-button" type="default">提交</button>
+			</view>
 		</c-blank-bar>
 	</view>
 </template>
@@ -39,6 +46,10 @@
 </script>
 
 <style lang="scss">
-	
+	.c-button{
+		height: 80rpx;
+		font-size: 32rpx;
+		border-radius: 80rpx;
+	}
 </style>
 
